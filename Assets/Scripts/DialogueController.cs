bae289c7afc337ea627cb8c43a593a4c96c311ef
nail00749 <71979155+nail00749@users.noreply.github.com>
2021-05-18 22:38:33 +0500

    dialogue add playbaleDirector

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
@@ -20,10 +21,11 @@ public class DialogueController : MonoBehaviour
         "Движение вытянутой рукой, ладонью В СТОРОНУ ТРЕБУЕМОГО ДВИЖЕНИЯ крана или моста. Стропальщик должен стоять лицом к машинисту крана. Если нужно передвинуть кран в другую сторону, то меняется рука, т.к. направление движения должно выходить из ладони",
         "Стропальщик стоит боком к машинисту крана. Движение рукой, согнутой в локте, ладонью В СТОРОНУ ТРЕБУЕМОГО ДВИЖЕНИЯ тележки. Если нужно передвинуть тележку в другую сторону, то меняется рука, т.к. направление движения должно выходить из ладони.",
     };
+    
     public TextMeshProUGUI text;
-
-    public List<PlayableDirector> timelines;
+    public List<TimelineAsset> timelines;
     private int counter = 0;
+    public PlayableDirector director;
     
     public void StartLesson()
     {
@@ -36,6 +38,8 @@ public class DialogueController : MonoBehaviour
         {
             counter++;
             text.text = dialogueText[counter];
+            var selectedAsset = timelines[counter];
+            director.Play(selectedAsset);
         }
     }
 }
