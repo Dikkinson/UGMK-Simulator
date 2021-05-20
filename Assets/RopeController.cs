0fbbc4152964b1773c73812de0d2f853d817606a
Sergey Shibaev <Dikkinsan@yandex.ru>
2021-05-20 14:23:00 +0500

    Чистка вилкой ненужно хлама

---
+++ Assets/RopeController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour
+{
+    public bool setActive;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "rope")
+        {
+            other.GetComponent<MeshRenderer>().enabled = setActive;
+            //other.gameObject.SetActive(setActive);
+            
+        }
+    }
+}
