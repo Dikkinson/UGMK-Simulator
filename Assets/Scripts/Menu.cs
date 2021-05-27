9b89049453bf462e9b751f1746742a0b6c9e445c
nail00749 <71979155+nail00749@users.noreply.github.com>
2021-05-27 18:02:07 +0500

    Script menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Gesture()
+    {
+        SceneManager.LoadScene("UGMK");
+    }
+
+    public void Stroils()
+    {
+        SceneManager.LoadScene("Stropili");
+    }
+
+    public void FinalTest()
+    {
+        SceneManager.LoadScene("FinalTest");
+    }
+}
