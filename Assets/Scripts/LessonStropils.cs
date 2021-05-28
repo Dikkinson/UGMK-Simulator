acb1e42c6896685e004ab043299434be02052a15
Sergey Shibaev <Dikkinsan@yandex.ru>
2021-05-28 18:20:03 +0500

    За себя и за Сашку

--- Assets/Scripts/LessonStropils.cs
+++ Assets/Scripts/LessonStropils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ItemRotationScript : MonoBehaviour
+public class LessonStropils : MonoBehaviour
 {  
     
     public List<GameObject> stropili;
